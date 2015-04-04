5637df34b7e4b26fdc92f9182097a7b5bb0e1154
feitian124 <feitian124@gmail.com>
2015-04-04 19:07:01 +0800

    get /posts public access

--- server/config/policies.js
+++ server/config/policies.js
@@ -33,6 +33,9 @@ module.exports.policies = {
   },
   AuthController: {
       '*': true,
+  },
+  PostController: {
+    'find': true
   }
 
   /***************************************************************************
